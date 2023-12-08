1d57b67e42676658d6c616915d999b7f79ddbee9
Kaemri <70956461+KlaidasKaralevicius@users.noreply.github.com>
2023-12-08 11:08:35 +0200

    Add files via upload

--- web.js
+++ web.js
@@ -6,7 +6,13 @@ function calculate() {
     };
 
     if (isNaN(form_data.number) || form_data.number < 10 || form_data.number > 99) {
-        alert('Enter two-digit number');
+        alert('Galima įvesti tik dviženklį skaičių');
+        return;
+    } else if (form_data.name == 0) {
+        alert('Įveskite vardą');
+        return;
+    } else if (form_data.surname == 0) {
+        alert('Įveskite pavardę');
         return;
     }
 
@@ -14,11 +20,11 @@ function calculate() {
     const multiplied_num = Math.round(Math.sqrt(random_num * form_data.number));
 
     const display_data = `
-        Name: ${form_data.name}
-        Surname: ${form_data.surname}
-        Selected number: ${form_data.number}
-        Random number: ${random_num}
-        ${form_data.name} ${form_data.surname} rolled ${multiplied_num}
+        Vardas: ${form_data.name}
+        Pavardė: ${form_data.surname}
+        Pasirinktas skaičius: ${form_data.number}
+        Sugeneruotas skaičius: ${random_num}
+        ${form_data.name} ${form_data.surname} gavo ${multiplied_num}
     `;
 
     const display_area = document.getElementById('display');
@@ -33,6 +39,6 @@ function calculate() {
     }
 
     console.log(form_data);
-    console.log(`Random number: ${random_num}`);
-    console.log(`Multiplication result: ${multiplied_num}`);
+    console.log(`Sugeneruotas skaičius: ${random_num}`);
+    console.log(`Daugybos rezultatas: ${multiplied_num}`);
 }
